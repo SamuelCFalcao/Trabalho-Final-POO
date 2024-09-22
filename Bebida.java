f9a36978aad89384c9fca8861505dcf87608803d
D00dinka <duardamt@gmail.com>
2024-09-22 19:48:52 -0300

    Implementação inicial Estoque + Bebida
    Implementação inicial das classes Bebida (subclasse de Produto) e Estoque (CRUD do projeto).
    Implementação da classe Main vazia, apenas o esqueleto da classe.

--- Bebida.java
+++ Bebida.java
@@ -1,6 +1,18 @@
 public class Bebida extends Produto {
-    private boolean alcoolica;
+    //Define se é uma bebida quente ou fria
+    private boolean quente;
 
-    public void servir() {
+    //Sobrescreve método de Produto para mostrar em mL
+    @Override
+    public void calculaPeso(float peso) {
+        //Considerando a entrada como em mL
+        //Se o peso for maior ou igual a 1000 mL
+        if (peso >= 1000){
+            peso = peso/1000;
+            System.out.println(peso+"L");
+        }
+        else{
+            System.out.println(peso+"mL");
+        }
     }
 }
---
+++ Estoque.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Estoque{
+	//Variável de Scanner para ler inputs
+	//private static Scanner scanner = new Scanner(System.in);
+
+    //Variável interna para ter uma lista de produtos
+    private static List<Produto> produtos = new ArrayList<>();
+	
+	public static void create(int a) {
+        //Verifica se é Salgado, Bebida ou Bombom
+        if (a == 1){
+            Salgado produto = new Salgado();
+        }
+        else if (a == 2){
+            Bebida produto = new Bebida();
+        }
+        else if (a == 3){
+            Bombom produto = new Bombom();
+        }
+
+        produto.setId();
+        produto.setNome();
+        produto.setPreco();
+
+		//Adiciona o produto à lista
+        this.produtos.add(produto);
+
+		System.out.println("Usuário inserido! Voltando ao menu!");
+	}
+
+	public static void read() {
+        //Lista todos os produtos
+		for (Produto p: this.produtos) {
+			System.out.println(p);
+		}
+	}
+	
+	public static void update(int a) {
+        //Verifica se é Salgado, Bebida ou Bombom
+        if (a == 1){
+            Salgado produto = new Salgado();
+        }
+        else if (a == 2){
+            Bebida produto = new Bebida();
+        }
+        else if (a == 3){
+            Bombom produto = new Bombom();
+        }
+
+        System.out.println("Forneça o ID do usuário a ser modificado: ");
+		String id = scanner.next();
+		int idInteiro = Integer.parseInt(id);
+		
+		System.out.println("Qual o novo nome? ");
+		String nome = scanner.next();
+
+        System.out.println("Qual preço? ");
+		String preco = scanner.next();
+
+        //Variável que irá checar se aquele ID pertence a algum usuário existente
+		boolean noID = true;
+
+        for (Produto p: this.produtos) {
+		    if (p.getId() == idInteiro) {
+                p.setNome(nome);
+                p.setPreco(preco);
+                noID = false;
+                break;
+            }
+		}
+	    
+		//Caso não existe nenhum usuário com o ID
+		if(noID) {
+			System.out.println("Nenhum usuário encontrado com este ID!");
+		}
+        
+		//Caso exista um usuário com o ID
+		else {
+			System.out.println("Usuário atualizado! Voltando ao menu!");
+		}
+	}
+
+	public static void delete() {	
+		System.out.println("Forneça o ID do usuário a ser modificado: ");
+		String id = scanner.next();
+		int idInteiro = Integer.parseInt(id);
+		
+		
+		//Verifica se existe um usuário com aquele ID, se houver, remove
+		produtos.removeIf(p -> p.getId() == idInteiro);
+
+		System.out.println("Usuário removido! Voltando ao menu!");
+	}
+}
