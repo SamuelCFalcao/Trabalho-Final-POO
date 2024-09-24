8182835ba71d2a776dd56a1067023a1ddc41c980
Samuel Falcão <samuelfalcao@alu.ufc.br>
2024-09-24 18:27:49 -0300

    Update Salgado.java

--- Salgado.java
+++ Salgado.java
@@ -2,11 +2,11 @@ public class Salgado {
     public String sabor;
     public boolean fritura;
 
-    public void removerProduto(int quantidade){
-        Estoque.removerProduto(quantidade);
+    public void removerProduto(String nome){
+        Estoque.removerProduto(nome);
     }
 
-    public void calculaPeso(float peso) {
+    public void calculaVolume(float peso) {
         if(peso >= 1000) {
             peso = peso/1000;
             System.out.println(peso + " kg");
